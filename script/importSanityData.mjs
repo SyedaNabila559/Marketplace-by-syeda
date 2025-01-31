cc1b728752a275c6107a5536cb5d5918838d68f6
SyedaNabila559 <abidkhan78000@gmail.com>
2025-01-31 00:57:17 -0800

    first commit

--- script/importSanityData.mjs
+++ script/importSanityData.mjs
@@ -1,11 +1,11 @@
 import { createClient } from '@sanity/client';
 
 const client = createClient({
-  projectId: 'your-project-id',
+  projectId: '2f24lzh2',
   dataset: 'production',
   useCdn: true,
   apiVersion: '2025-01-13',
-  token: 'your-auth-token',
+  token: 'skMp2YNFvX4FfeUq7O3grXQ5xKetyjHU7uWiZgIp98nhbrblGgNrcKrABh49NHdA5Vb9rdvCRIcUzjuUCZI39OhW02v3UgLeEP592D0QXIAG2ePwEv5mf1Zv8URCCreKWuGZdsKxHvW25Lq3yyUSRLJwNMr4MbzX3khr2gleZrR6XJiCZ0TO',
 });
 
 async function uploadImageToSanity(imageUrl) {
